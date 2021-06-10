86301898b84aa91554555a041836692ad81ec6e4
rooparsh <rooparshkalia@gmail.com>
2021-06-10 11:02:47 +0530

    removed duplicate classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ buildscript {
     }
     dependencies {
         classPath(ProjectConfig.classPathList)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
